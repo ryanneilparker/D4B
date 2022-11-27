ddb6c19d647c001b0cdb68b6d4a742577205359e
ryanneilparker <ryanneilparker@gmail.com>
2022-11-27 11:48:09 +0200

    Add user authentication

--- news/accounts/forms.py
+++ news/accounts/forms.py
@@ -7,12 +7,12 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+        fields = ('username', 'email', 'age',)
 
 
 class CustomUserChangeForm(UserCreationForm):
 
     class Meta(UserChangeForm):
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'email', 'age')
 
